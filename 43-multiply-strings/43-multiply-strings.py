0c7338f90dd6ad4b19451c80f69e7eed83bdc0d8
Suvendu Chowdhury <105709770+SUVENDUC789@users.noreply.github.com>
2022-09-26 15:31:48 +0530

    Time: 41 ms (53.69%), Space: 13.6 MB (52.67%) - LeetHub

---
+++ 43-multiply-strings/43-multiply-strings.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        num1=int(num1)
+        num2=int(num2)
+        num=num1*num2
+        num=str(num)
+        return num
+        

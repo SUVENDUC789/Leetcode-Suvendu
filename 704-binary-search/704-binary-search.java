e513a891a220f985cbea46bb58e13a5b6b36f534
Suvendu Chowdhury <105709770+SUVENDUC789@users.noreply.github.com>
2022-09-30 14:48:00 +0530

    Time: 0 ms (100.00%), Space: 54.4 MB (44.57%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -3,14 +3,14 @@ class Solution {
         int low=0, high=nums.length-1 ,index=-1;
         while(low<=high){
             int mid=(low+high)/2;
-            if (nums[low]==target){
-                return low;
-            }
-            else if(nums[high]==target){
-                return high;
-            }
+            // if (nums[low]==target){
+            //     return low;
+            // }
+            // else if(nums[high]==target){
+            //     return high;
+            // }
             
-            else if(nums[mid]==target){
+            if(nums[mid]==target){
                 return mid;
             }
             else if(nums[mid]>target)

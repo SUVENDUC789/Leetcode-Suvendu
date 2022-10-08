e7f21b63d65cace3a929a717b3ce1ad8deab5237
Suvendu Chowdhury <105709770+SUVENDUC789@users.noreply.github.com>
2022-10-08 15:17:24 +0530

    Time: 8 ms (5.22%), Space: 73.1 MB (47.01%) - LeetHub

---
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] a) {
+        int max=a[0];
+        int index=0;
+        for(int i=1;i<a.length;i++){
+            if(max<a[i]){
+                max=a[i];
+                index=i;
+            }
+        }
+        return index;
+        
+    }
+}

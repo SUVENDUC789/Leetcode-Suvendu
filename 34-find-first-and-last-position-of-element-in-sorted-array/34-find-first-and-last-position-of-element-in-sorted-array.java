af3eb0e3855943ff62a979ab7ff781b097ec5c04
Suvendu Chowdhury <105709770+SUVENDUC789@users.noreply.github.com>
2022-10-09 20:23:46 +0530

    Time: 0 ms (100.00%), Space: 47.3 MB (38.21%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,31 +1,34 @@
 class Solution {
-    public static int count(int[] a, int target) {
-        int c = 0;
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] == target) {
-                c++;
+    public static int bs(int []a , int i, boolean findstartindex){
+        int ans=-1;
+        int low=0;
+        int high=a.length-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(a[mid]==i){
+               ans=mid;
+                if(findstartindex){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+            else if(a[mid]>i){
+                high=mid-1;
+            }else{
+               low=mid+1; 
             }
         }
-        return c;
+        return ans;
     }
-    public int[] searchRange(int[] a, int target) {
-
-        int[] b = new int[count(a, target)];
-        int k = 0;
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] == target) {
-                b[k] = i;
-                k++;
-            }
-        }
-        int[] c = {-1,-1 };
-        if(count(a, target)!=0){
-            c[0]=b[0];
-            c[1]=b[b.length-1];
-        }
+    public int[] searchRange(int[] nums, int target) {
         
-        // System.out.println(Arrays.toString(c));
-        return c;
-        
+        int []ans={-1,-1};
+        int start=bs(nums,target,true);
+        int end=bs(nums,target,false);
+        ans[0]=start;
+        ans[1]=end;
+        return ans;
     }
 }

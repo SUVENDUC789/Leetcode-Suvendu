161a2c7d3e0b98c53b4fb58c01b7ad3406a2a950
Suvendu Chowdhury <105709770+SUVENDUC789@users.noreply.github.com>
2022-10-12 20:01:01 +0530

    Time: 1 ms (43.39%), Space: 42.2 MB (76.95%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean searchMatrix(int[][] a, int target) {
+    for(int []x:a){
+        for(int i:x){
+            if(i==target){
+                return true;
+            }
+        }
+    }
+        return false;
+        
+    }
+}

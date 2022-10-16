beac3c84c4cd8aacf1ac9605909029aa7ed769cb
Suvendu Chowdhury <105709770+SUVENDUC789@users.noreply.github.com>
2022-10-16 21:58:43 +0530

    Time: 3 ms (9.34%), Space: 42.7 MB (34.93%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void sortColors(int[] a) {
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<a.length-1-i;j++){
+                if(a[j]>a[j+1]){
+                    int temp=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
+                }
+            }
+        }
+        
+    }
+}

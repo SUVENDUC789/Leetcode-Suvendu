9fb0272985a4218a48052e23a6161b1b228a8924
Suvendu Chowdhury <105709770+SUVENDUC789@users.noreply.github.com>
2022-10-16 21:47:03 +0530

    Time: 4 ms (22.45%), Space: 50.2 MB (38.06%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.java
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -3,10 +3,11 @@ class Solution {
         int []b=new int[2*a.length];
         for(int i=0;i<a.length;i++){
             b[i]=a[i];
+            b[i+a.length]=a[i];
         }
-        for(int i=a.length;i<2*a.length;i++){
-            b[i]=a[i-a.length];
-        }
+        // for(int i=a.length;i<2*a.length;i++){
+        //     b[i]=a[i-a.length];
+        // }
         return b;
     }
 }

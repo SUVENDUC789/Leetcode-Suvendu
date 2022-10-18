8c12caf17922f8bbf846769a08791519ec4078a2
Suvendu Chowdhury <105709770+SUVENDUC789@users.noreply.github.com>
2022-10-18 23:23:12 +0530

    Time: 66 ms (48.22%), Space: 13.9 MB (83.31%) - LeetHub

---
+++ 2325-decode-the-message/2325-decode-the-message.py
@@ -0,0 +1,25 @@
+class Solution:
+    def decodeMessage(self, key: str, message: str) -> str:
+        map123="abcdefghijklmnopqrstuvwxyz"
+        key=key.replace(" ","")
+
+        a= {}
+        i=0
+        for item in key:
+            a[item]=-1
+
+        for key1,value in a.items():
+            a[key1]=map123[i]
+            i=i+1
+
+        # print(a)
+        newMessage=''
+        for i in message:
+            if i != ' ':
+                newMessage=newMessage+a[i]
+            else:
+                newMessage=newMessage+' '
+
+
+        return newMessage
+        

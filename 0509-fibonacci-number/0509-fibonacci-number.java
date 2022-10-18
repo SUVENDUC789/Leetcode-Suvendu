a01f7fcc83113d17fc700e3139c25800974dfbe1
Suvendu Chowdhury <105709770+SUVENDUC789@users.noreply.github.com>
2022-10-18 09:43:39 +0530

    Time: 20 ms (14.26%), Space: 41.5 MB (10.12%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,10 +1,9 @@
 class Solution {
     public int fib(int n) {
-        if(n==0)return 0;
-        else if(n==1)return 1;
-        else if(n>1) {
-            return fib(n-1)+fib(n-2);
-        }
+       if(n==0)return 0;
+        if(n==1)return 1;
+        // if(n==2)return 1;
         return fib(n-1)+fib(n-2);
+        
     }
 }

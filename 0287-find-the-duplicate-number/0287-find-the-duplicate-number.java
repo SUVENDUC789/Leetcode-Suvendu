fe363e1a6259f760ab733f3d9295592805a520c6
Suvendu Chowdhury <105709770+SUVENDUC789@users.noreply.github.com>
2022-10-29 12:44:45 +0530

    Time: 2 ms (99.84%), Space: 61.1 MB (84.92%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int max=nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(max<nums[i]){
+                max=nums[i];
+            }
+        }
+        
+        int [] hsh = new int [max+1];
+        
+        for(int i=0;i<nums.length;i++){
+            hsh[nums[i]]++;
+        }
+        
+        
+        
+        for(int i=0;i<nums.length;i++){
+            if(hsh[nums[i]]>1){
+                return nums[i];
+            }
+        }
+        
+        return 0;
+    }
+}

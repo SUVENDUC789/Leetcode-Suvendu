38d90c2249f85d2b0eee92456d63f44fdb001da8
Suvendu Chowdhury <105709770+SUVENDUC789@users.noreply.github.com>
2022-10-30 23:03:11 +0530

    Time: 4 ms (54.25%), Space: 42.7 MB (43.63%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.java
+++ 1816-truncate-sentence/1816-truncate-sentence.java
@@ -1,6 +1,9 @@
 class Solution {
     public String truncateSentence(String s, int k) {
         String[] arrOfStr = s.split(" ");
+        if(arrOfStr.length==k){
+            return s;
+        }
         String[] newstr=new String[k];
         for(int i=0;i<k;i++){
             newstr[i]=arrOfStr[i];

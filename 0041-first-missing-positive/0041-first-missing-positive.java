2585bd18d90c14e0bedc2d02e7d9cfc3181cf333
Suvendu Chowdhury <105709770+SUVENDUC789@users.noreply.github.com>
2022-11-04 18:56:26 +0530

    Time: 38 ms (9.70%), Space: 51 MB (87.32%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,27 @@
+class Solution {
+    private static boolean find(int[]a,int num){
+        int low=0;
+        int high=a.length-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(a[mid]==num){
+                return true;
+            }else if(a[mid]>num){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return false;
+    }
+    public int firstMissingPositive(int[] nums) {
+        Arrays.sort(nums);
+        int max=nums.length+1;
+        for(int i=1;i<=max;i++){
+            if(!find(nums,i)){
+                return i;
+            }
+        }
+        return 1;
+    }
+}

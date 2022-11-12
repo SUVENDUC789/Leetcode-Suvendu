0527428ff48d128094db5b2d2a94125b26fc3864
Suvendu Chowdhury <105709770+SUVENDUC789@users.noreply.github.com>
2022-11-12 23:59:08 +0530

    Time: 807 ms (40.69%), Space: 91.9 MB (43.45%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.c
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.c
@@ -36,12 +36,18 @@ struct ListNode* mergeNodes(struct ListNode* head){
     struct ListNode*q=head;
     struct ListNode*s=head;
     int size=0;
+    
+    // Size of linklist 
     while(p!=NULL){
         size++;
         p=p->next;
     }
+    
+    // Linklist size = array size 
     int *a=(int*)calloc(size,sizeof(int));
     int k=0;
+    
+    // list all value copy in array
     while(q!=NULL){
         a[k]=q->val;
         k++;
@@ -49,8 +55,14 @@ struct ListNode* mergeNodes(struct ListNode* head){
     }
     
     int ln=0,sum=0;
+    
+    // Itterative all array 
     for(int i=0;i<size;i++){
+        
+        // if array element is 0 
         if(a[i]==0){
+            
+            // sum value cpy in list 
             s->val=sum;
             sum=0;
             ln=1;
@@ -66,8 +78,14 @@ struct ListNode* mergeNodes(struct ListNode* head){
     }
     
     s->next=NULL;
+    
+    // free total array 
     free(a);
+    
+    // delele at first node 
     head=DeleteAtfastNode(head);
+    
+    // delete at last node 
     head=DeleteAtLastNode(head);
        
     

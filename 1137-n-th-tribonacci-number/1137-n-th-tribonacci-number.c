b4765d39d4ac577451b0f4dd1fc5f408fdc99373
Suvendu Chowdhury <105709770+SUVENDUC789@users.noreply.github.com>
2022-11-15 09:00:14 +0530

    Time: 5 ms (20.53%), Space: 5.4 MB (97.68%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.c
@@ -0,0 +1,13 @@
+
+int dp[40];
+int tribonacci(int n){
+    if(n==0)return 0;
+    else if(n==1) return 1;
+    else if(n==2) return 1;
+    else if(dp[n]!=0)return dp[n];
+    
+    dp[n]=tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
+    return dp[n];
+    return 0;
+
+}

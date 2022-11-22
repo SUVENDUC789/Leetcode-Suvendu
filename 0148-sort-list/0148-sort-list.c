d42032019963206f9eff5015024caa2224ffd7aa
Suvendu Chowdhury <105709770+SUVENDUC789@users.noreply.github.com>
2022-11-22 08:46:01 +0530

    Time: 99 ms (88.80%), Space: 20.9 MB (36.10%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.c
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+int SizeofList(struct ListNode *p)
+{
+    int c = 0;
+    while (p)
+    {
+        c++;
+        p = p->next;
+    }
+    return c;
+}
+int comp(const void *a, const void *b)
+{
+    const int *pa = a;
+    const int *pb = b;
+    return *pa - *pb;
+}
+struct ListNode* sortList(struct ListNode* head){
+    struct ListNode *p = head;
+    struct ListNode *q = head;
+    struct ListNode *f = head;
+    int n = SizeofList(p);
+    // printf("Size of list is : %d\n", n);
+    int *a = (int *)calloc(n, sizeof(int));
+    int k = 0;
+    while (q != NULL)
+    {
+        a[k++] = q->val;
+        q = q->next;
+    }
+    qsort(a, n, sizeof(int), comp);
+    k = 0;
+    while (f)
+    {
+        f->val = a[k++];
+        f = f->next;
+    }
+    // for(int i=0;i<n;i++)
+    // {
+    //     printf("%d\n",a[i]);
+    // }
+    return head;
+}
